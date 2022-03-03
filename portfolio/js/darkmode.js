e530f0ca6ee07084b070d81619957c99c334166b
JeannePacard <97599809+JeannePacard@users.noreply.github.com>
2022-03-03 16:57:49 +0100

    Delete portfolio directory

--- portfolio/js/darkmode.js
+++
@@ -1,6 +0,0 @@
-const html = document.getElementsByTagName("html")[0];
-const themeSwitch = document.getElementById("switch");
-
-themeSwitch.addEventListener("click", () => {
-  html.classList.toggle("dark");
-});
